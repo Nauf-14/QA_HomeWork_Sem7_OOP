da3f3a0970cfdb0d4a5fd00f0b3d3c0fd47c4513
Naufal <nauf1207@mail.ru>
2024-02-27 21:25:38 +0600

    LogComplexNumberFactory

---
+++ LogComplexNumberFactory.java
@@ -0,0 +1,15 @@
+package QA_HomeWork_Sem7_OOP;
+
+public class LogComplexNumberFactory implements IComplexNumberFactory {
+
+    private Loggable logger;
+
+    public LogComplexNumberFactory(Loggable logger) {
+        this.logger = logger;
+    }
+
+    @Override
+    public ComplexNumber create(int real, int imaginary) {
+        return new LogComplexCalculator(new ComplexCalculator(real, imaginary), logger);
+    }
+}
